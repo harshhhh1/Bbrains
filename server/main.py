c055a34d5166bd5c4a5c2c110be330902ddb731a
harshhhh1 <hp786060@gmail.com>
2025-11-12 21:18:47 +0530

    added files

---
+++ server/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/submit-data', methods=['POST'])
+def submit_data():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return jsonify(success=True, username=username)
+if __name__ == '__main__':
+    app.run(debug=True)
