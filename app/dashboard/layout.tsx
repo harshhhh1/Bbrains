1625e930b9abd79bcc3cc504ed662ee99e78880e
harshhhh1 <hp786060@gmail.com>
2025-12-31 13:17:30 +0530

    Add dashboard layout and page with navigation link
    Introduces a new dashboard section with its own layout and page components. Also adds a navigation link to the dashboard from the home page.

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useState } from "react";
+export default function DashboardLayout(){
+    return(
+        <>
+        Hello this is dashboard
+        </>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage() {
+  return (
+    <h1>Welcome to your Dashboard!</h1>
+  );
+}
