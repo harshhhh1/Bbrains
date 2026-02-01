0bfcb1844cb7e28ecc7e793783867809d706448a
harshhhh1 <hp786060@gmail.com>
2026-02-01 20:22:41 +0530

    deleted old files

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
- import express from 'express'
- const router = express.Router();
-
- import {signin, signup, userdata} from '../controllers/auth.js';
- // Define your auth routes here
- router.post('/signup',signup);
- router.post('/signin',signin);
- router.get('/dashboard',userdata);
- export default router;           
