d96e555ece8ff427d443be0a408ecade2d1f8ec7
harshhhh1 <hp786060@gmail.com>
2026-02-01 20:22:41 +0530

    deleted old files

--- routes/products.js
+++
@@ -1,9 +0,0 @@
- import express from 'express'
- const router = express.Router();
-
- import {products} from '../controllers/products.js';
- // Define your auth routes here
- router.get('/products',products);
- router.post('/products:id',products);
- 
- export default router;           
